c0165e9bccb59903666ab4931a353a5e354e4822
Sarah Porsch Milani <sarahporsch25@gmail.com>
2024-09-10 23:24:14 -0300

    fundamentos java 02

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
-
-const mensagem = "ola mundo!";
+//constantes, variaveis e escopos. 
+/*const mensagem = "ola mundo!";
 
 {
-    const mensagem = "ola eu";
+    let mensagem = "ola eu";
     console.log(mensagem);
 }
 
 console.log(mensagem);
+*/
+
+//------------------------------------------
+
+/*arrays e objetos
+let metas = ["Sarah", "Inter"];
+
+console.log(metas[1]);
+
+//concatenação 
+console.log(metas[0] + " é " + metas[1]);
+*/
+//objetos
+let meta = {
+    value: 'terminar trabalho de csharp',
+    adress: 2,
+    checked: true,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+meta.value = "agora é terminar meus livros"
+meta.log(meta.value); 
+
+
+//funçoes
+const criarMeta = () => {}
+
+//function criarMeta() {}
