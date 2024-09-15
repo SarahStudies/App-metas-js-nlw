32333df37fe81a209eb819d868fff9e565b2acf1
Sarah Porsch Milani <sarahporsch25@gmail.com>
2024-09-15 01:33:53 -0300

    json + app completo!

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
 var mensagem = "Bem vindo ao app de metas"
-let meta = {
-    value: 'Tomar 3L de água por dia',
-    checked: false,
+
+
+let metas 
+
+const carregarMetas = async () => {
+    try {
+        const dados = await fs.readFile("metas.json", "utf-8")
+        metas = JSON.parse(dados)
+    } catch (erro) {
+        metas = []
+    }
 }
 
-let metas = [ meta ]
+const salvarMetas = async () => {
+    await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta: "})
@@ -139,8 +150,11 @@ const mostrarMensagens = () => {
 }
 
 const start = async () => {
+    await carregarMetas()
+
     while(true){
-        mostrarMensagens();
+        mostrarMensagens()
+        await salvarMetas()
 
         const opcao = await select({
             message: "Menu >",
